58dbc7cf31c5f5c951c0616615b308585da4d984
Eduardo Batista <eduardo_batistaalves@hotmail.com>
2024-05-29 16:36:51 -0300

    feat: create a code to insert mask on phone number form

--- libs/elementor/form-v1/form.js
+++ libs/elementor/form-v1/form.js
@@ -1,1 +1,28 @@
-//text here
+//Aplica máscara a campo phone formulário elementor
+
+document.addEventListener("DOMContentLoaded", function() {
+    var phoneInput = document.getElementById('form-field-phone');
+    var streetInput = document.getElementById('form-field-postal_code');
+  
+    phoneInput.addEventListener('input', function() {
+      var phone = phoneInput.value.replace(/\D/g, '');
+  
+      if(phone.length > 0) {
+        phone = '(' + phone.substring(0, 2) + ')' + phone.substring(2);
+      }
+      if(phone.length > 3) {
+        phone = phone.substring(0, 3) + ' ' + phone.substring(3);
+      }
+      if(phone.length > 10) {
+        phone = phone.substring(0, 10) + '-' + phone.substring(10, 14);
+      }
+      phoneInput.value = phone;
+    });
+  
+    streetInput.addEventListener('blur', function() {
+      var phone = phoneInput.value;
+      phone = phone.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+      phoneInput.value = phone; // Define o valor sem a máscara
+    });
+  });
+  
