11188b332758f6ad4f775ce91c489c105bcbf78d
Eduardo Batista <eduardo_batistaalves@hotmail.com>
2024-06-05 10:11:30 -0300

    feat: create the phone2 with no mask and fix backspace error

--- libs/elementor/form-v1/form.js
+++ libs/elementor/form-v1/form.js
@@ -1,28 +1,29 @@
 //Aplica máscara a campo phone formulário elementor 
 
 function phone() {
-    var phoneInput = document.getElementById('form-field-phone');
-    var streetInput = document.getElementById('form-field-postal_code');
-  
-    phoneInput.addEventListener('input', function() {
-      var phone = phoneInput.value.replace(/\D/g, '');
-  
-      if(phone.length > 0) {
-        phone = '(' + phone.substring(0, 2) + ')' + phone.substring(2);
+  var phoneInput = document.getElementById('form-field-phone');
+  var phoneInput2 = document.getElementById('form-field-phone2'); // Novo campo
+
+  phoneInput.addEventListener('input', function(e) {
+      let phone = phoneInput.value.replace(/\D/g, '');
+      let formattedPhone = '';
+
+      if (e.inputType === 'deleteContentBackward') {
+          phoneInput.value = phone;
+      } else {
+          if (phone.length > 0) {
+              formattedPhone = '(' + phone.substring(0, 2) + ')';
+          }
+          if (phone.length > 2) {
+              formattedPhone += ' ' + phone.substring(2, 7);
+          }
+          if (phone.length > 7) {
+              formattedPhone += '-' + phone.substring(7, 11);
+          }
+          phoneInput.value = formattedPhone;
       }
-      if(phone.length > 3) {
-        phone = phone.substring(0, 3) + ' ' + phone.substring(3);
-      }
-      if(phone.length > 10) {
-        phone = phone.substring(0, 10) + '-' + phone.substring(10, 14);
-      }
-      phoneInput.value = phone;
-    });
-  
-    streetInput.addEventListener('blur', function() {
-      var phone = phoneInput.value;
-      phone = phone.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
-      phoneInput.value = phone; // Define o valor sem a máscara
-    });
-  };
+      // Atualizar o campo form-field-phone2 sem caracteres especiais
+      phoneInput2.value = phone;
+  });
+}
   
